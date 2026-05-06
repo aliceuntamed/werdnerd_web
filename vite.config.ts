2599d6ed4971a3834111c4a2665237824d3596e5
Stephanie <skalynn22@gmail.com>
2026-05-06 18:26:42 -0500

    reset

--- vite.config.ts
+++ vite.config.ts
@@ -1,26 +1,28 @@
 /// <reference types="vitest/config" />
-import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite'
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import tailwindcss from "@tailwindcss/vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
-import path from 'node:path';
-import { fileURLToPath } from 'node:url';
-import { playwright } from '@vitest/browser-playwright';
-const dirname = typeof __dirname !== 'undefined' ? __dirname : path.dirname(fileURLToPath(import.meta.url));
-
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import { playwright } from "@vitest/browser-playwright";
+const dirname =
+  typeof __dirname !== "undefined"
+    ? __dirname
+    : path.dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
-      '@': path.resolve(dirname, './src'),
+      "@": path.resolve(dirname, "./src/*"),
     },
   },
   test: {
     globals: true,
-    environment: 'jsdom',
-    setupFiles: './src/setupTests.ts',
+    environment: "jsdom",
+    setupFiles: "./src/setupTests.ts",
     browser: {
       enabled: true,
       provider: playwright(),
