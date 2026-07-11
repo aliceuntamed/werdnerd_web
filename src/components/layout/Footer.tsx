01386e7090d2e047d4f50288a4545359891fb235
Stephanie <124111589+aliceuntamed@users.noreply.github.com>
2026-07-11 11:33:16 -0500

    Fix newsletter markup formatting

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -165,7 +165,11 @@ export function Footer() {
                 Stay Curious
               </label>
 
-              <p className="site-footer__newsletter-copy">\n                Get a <em>daily dose</em> of <em>linguistic levity</em> sent to your inbox.\n              </p>\n\n              <div className="site-footer__form-row">
+              <p className="site-footer__newsletter-copy">
+                Get a <em>daily dose</em> of <em>linguistic levity</em> sent to your inbox.
+              </p>
+
+              <div className="site-footer__form-row">
                 <div className="site-footer__input-wrap">
                   <input
                     id="footer-email"
